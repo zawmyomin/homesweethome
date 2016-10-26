184c4f42bb2982e08d4e5f1b733648136cfb5047
zaw <zmm.justin@gmail.com>
2016-10-26 10:30:57 +0630

    Initial Commit

---
+++ homesweethome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homesweethome
+//
+//  Created by Zaw Myo Min on 26/10/16.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
